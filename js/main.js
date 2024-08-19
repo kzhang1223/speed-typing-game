64bc848723d6884abe7acf5cc0dea1f4c4f7e686
Kathy <kzhang1223@gmail.com>
2024-08-19 10:59:14 -0700

    included levels and fixed the score keeping

--- js/main.js
+++ js/main.js
@@ -1,7 +1,18 @@
 window.addEventListener('load', init);
 
 // Global variables
-let time = 5;
+
+// Available levels
+const levels = {
+    easy: 5,
+    medium: 3,
+    hard: 1
+};
+
+// to change level 
+const currentLevel = levels.hard;
+
+let time = currentLevel;
 let score = 0;
 let highScore = 0;
 let isPlaying;
@@ -14,6 +25,7 @@ const timeDisplay = document.querySelector('#time');
 const message = document.querySelector('#message');
 const seconds = document.querySelector('#seconds');
 const highScoreDisplay = document.querySelector('#highscore');
+const topText = document.querySelector('#lead');
 
 const words = [
     'hello',
@@ -45,15 +57,48 @@ const words = [
 
 // Initialize game
 function init() {
+    // show number of seconds in ui
+    seconds.innerHTML = currentLevel;
+    
     // load word from array
     showWord(words);
-    
+
+    // start matching on word input 
+    wordInput.addEventListener('input', startMatch);
+
     // call countdown every second
     setInterval(countdown, 1000);
 
     // check game status
     setInterval(checkStatus, 50);
 };
+
+// start match
+function startMatch() {
+    if (matchWords()) {
+        isPlaying = true;
+        time = currentLevel + 1;
+        showWord(words);
+        wordInput.value = '';
+        score++;   
+    }
+    if (score === -1) {
+        scoreDisplay.innerHTML = 0;
+    } else {
+        scoreDisplay.innerHTML = score;
+    }
+}
+
+// match current word to word input
+function matchWords() {
+    if (wordInput.value === currentWord.innerHTML) {
+        message.innerHTML = 'Correct!';
+        return true;
+    } else {
+        message.innerHTML = '';
+        return false;
+    }
+}
 
 // pick and show random word
 function showWord(words) {
@@ -82,5 +127,6 @@ function countdown() {
 function checkStatus() {
     if (!isPlaying && time === 0) {
         message.innerHTML = 'Game Over!';
+        score = -1;
     }
 }
